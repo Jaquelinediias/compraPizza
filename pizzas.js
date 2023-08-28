5216c6336dc2afc65118e74d3047bbcbab5c278c
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-08-28 16:01:59 -0300

    novos exercício

--- pizzas.js
+++ pizzas.js
@@ -1,10 +1,10 @@
 let pizzaJson = [
     {id:1,
-         name:'Mussarela',
-          img:'images/pizza.png',
-          price:20.19,
-          sizes:['100g', '530g', '860g'], 
-          description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+        name:'Mussarela',
+        img:'images/pizza.png',
+        price:20.19,
+        sizes:['100g', '530g', '860g'], 
+        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
 
     {id:2,
         name:'Calabresa',
